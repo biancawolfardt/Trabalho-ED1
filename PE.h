f2bc6e1b56ea187071b27a5e30692eeac681dde8
biancawolfardt <biancawolfardt@alunos.utfpr.edu.br>
2025-10-30 11:45:56 -0300

    Update PE.h

--- PE.h
+++ PE.h
@@ -14,8 +14,8 @@ int p_vazia(Pilha *p);
 int p_cheia(Pilha *p);
 int push_p(Pilha *p, URL url);
 int pop_p(Pilha *p, URL url);
+int remove_base(Pilha *p);
 int p_tam(Pilha *p);
 void free_p(Pilha *p);
 
 #endif;
-
